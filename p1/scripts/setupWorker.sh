fe29cd852df4fd9c40387339dd6b9f1c58e74a09
Jooma <jamie.gworld@hotmail.com>
2024-09-18 20:48:40 +0200

    updated setupWorker

--- p1/scripts/setupWorker.sh
+++ p1/scripts/setupWorker.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
 
 echo -e "\033[1;3;33m--- Server Worker script starting ---\033[0m"
-
-apt-get update -y && apt-get install -y curl
-
 echo -e "\033[1;32m--- Installing K3s ---\033[0m"
 
-curl -sfL https://get.k3s.io | K3S_URL="https://$SERVER_IP:6443" \
-	K3S_TOKEN_FILE="/vagrant/node-token" \
+curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 \
+    K3S_KUBECONFIG_MODE="644" \
+	K3S_TOKEN="/vagrant/node-token" \
 	INSTALL_K3S_EXEC="--flannel-iface=eth1" \
 	sh -
-
-echo "
-echo
-echo -e '\033[1mifconfig eth1 (ip addr | grep 'eth1:' -A 6):\033[0m'
-ip addr | grep 'eth1:' -A 6
-" >> /home/vagrant/.bashrc
-
-echo -e "\033[1;3;33m--- Server Worker script finished ---\033[0m"
+    sleep 10
 
 echo "\033[1;3;33m--- K3s agent installation complete on pbureeraSW ---\033[0m"
