6082883e95868cb1f042d8e14a80a40d3a450245
Jooma <jamie.gworld@hotmail.com>
2024-09-18 21:32:07 +0200

    updated setupWorker

--- p1/scripts/setupWorker.sh
+++ p1/scripts/setupWorker.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 echo -e "\033[1;3;33m--- Server Worker script starting ---\033[0m"
+
+apt-get update -y && apt-get install -y curl
+
 echo -e "\033[1;32m--- Installing K3s ---\033[0m"
 
 curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 \
