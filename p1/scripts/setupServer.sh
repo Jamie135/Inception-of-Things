b045e76b82b8bb5dc7b228f7f65852e5b2e90d0d
Jamie135 <Jamie135>
2024-09-20 15:41:40 +0200

    updated p1

--- p1/scripts/setupServer.sh
+++ p1/scripts/setupServer.sh
@@ -11,7 +11,7 @@ echo -e "\033[1;32m--- Installing K3s ---\033[0m"
 
 # --node-ip=$SERVER_IP: specifies the node's IP address which is set earlier in the Vagrantfile
 # --flannel-iface=eth1: allows K3s to use the eth1 network interface, which is critical for networking between the virtual machines on the private network
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --write-kubeconfig-mode=644 --node-ip 192.168.56.110 --flannel-iface eth1" sh -s -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --write-kubeconfig-mode=644 --node-ip=192.168.56.110 --flannel-iface=eth1" sh -
 
 # wait unitl the node-token file is created
 NODE_TOKEN="/var/lib/rancher/k3s/server/node-token"
--- p1/scripts/setupWorker.sh
+++ p1/scripts/setupWorker.sh
@@ -9,11 +9,7 @@ ip link set eth1 up
 
 echo -e "\033[1;32m--- Installing K3s ---\033[0m"
 
-curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 \
-    K3S_KUBECONFIG_MODE="644" \
-	K3S_TOKEN=$(sudo cat /vagrant/node-token) \
-	INSTALL_K3S_EXEC="--flannel-iface=eth1" \
-	sh -
-    sleep 10
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent --write-kubeconfig-mode=644 --node-ip=192.168.56.111 --flannel-iface=eth1 --server=https://192.168.56.110:6443 --token-file=/vagrant/node-token" sh -
+sleep 10
 
 echo -e "\033[1;3;34m--- K3s worker installation complete on pbureeraSW ---\033[0m"
