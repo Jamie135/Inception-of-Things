4bab6098d2e4c197b45b806f26e3438688eecef0
Jamie135 <Jamie135>
2024-09-20 18:53:08 +0200

    updated p1

--- p1/scripts/setupServer.sh
+++ p1/scripts/setupServer.sh
@@ -2,12 +2,15 @@
 
 echo -e "\033[1;3;34m--- Server script starting ---\033[0m"
 
-apt-get update && apt-get install -y curl net-tools
+# "--flannel-iface eth1" allows K3s to use the eth1 network interface,
+# which is critical for networking between the virtual machines on the private network
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--flannel-iface eth1" K3S_KUBECONFIG_MODE="644" sh -
 
-# --node-ip=$SERVER_IP: specifies the node's IP address which is set earlier in the Vagrantfile
-# --flannel-iface=eth1: allows K3s to use the eth1 network interface, which is critical for networking between the virtual machines on the private network
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip=192.168.56.110 --flannel-iface=eth1 --write-kubeconfig-mode=644" sh - 
-cp /var/lib/rancher/k3s/server/node-token /vagrant
-cp /etc/rancher/k3s/k3s.yaml /vagrant/
+while [ ! -e /var/lib/rancher/k3s/server/node-token ]; do
+    sleep 2
+done
+
+sudo cp /var/lib/rancher/k3s/server/node-token /vagrant
+sudo cp /etc/rancher/k3s/k3s.yaml /vagrant/
 
 echo -e "\033[1;3;34m--- Server installation complete on pbureeraS ---\033[0m"
--- p1/scripts/setupWorker.sh
+++ p1/scripts/setupWorker.sh
@@ -2,8 +2,6 @@
 
 echo -e "\033[1;3;34m--- Worker script starting ---\033[0m"
 
-apt-get update && apt-get install -y curl net-tools
-
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent --node-ip=192.168.56.111 --server=https://192.168.56.110:6443 --flannel-iface=eth1 --token-file=/vagrant/node-token" sh -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--flannel-iface eth1" K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=$(sudo cat /vagrant/node-token) K3S_KUBECONFIG_MODE="644" sh -
 
 echo -e "\033[1;3;34m--- Worker installation complete on pbureeraSW ---\033[0m"
