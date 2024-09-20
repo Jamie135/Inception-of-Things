07e31916b6478a21251f87db7edd8e75866469ae
Jamie135 <Jamie135>
2024-09-20 17:55:10 +0200

    updated p1

--- p1/scripts/setupServer.sh
+++ p1/scripts/setupServer.sh
@@ -2,27 +2,12 @@
 
 echo -e "\033[1;3;34m--- Server script starting ---\033[0m"
 
-apk update && apk add curl && apk add net-tools
-
-ip addr add 192.168.56.110/24 dev eth1
-ip link set eth1 up
-
-echo -e "\033[1;32m--- Installing K3s ---\033[0m"
+apt-get update && apt-get install -y curl net-tools
 
 # --node-ip=$SERVER_IP: specifies the node's IP address which is set earlier in the Vagrantfile
 # --flannel-iface=eth1: allows K3s to use the eth1 network interface, which is critical for networking between the virtual machines on the private network
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --write-kubeconfig-mode=644 --node-ip=192.168.56.110 --flannel-iface=eth1" sh -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip=192.168.56.110 --flannel-iface=eth1 --write-kubeconfig-mode=644" sh - 
+cp /var/lib/rancher/k3s/server/node-token /vagrant
+cp /etc/rancher/k3s/k3s.yaml /vagrant/
 
-# wait unitl the node-token file is created
-NODE_TOKEN="/var/lib/rancher/k3s/server/node-token"
-while [ ! -e ${NODE_TOKEN} ]; do
-    sleep 2
-done
-
-sudo cat ${NODE_TOKEN}
-sudo cp ${NODE_TOKEN} /vagrant/
-
-KUBE_CONFIG="/etc/rancher/k3s/k3s.yaml"
-sudo cp ${KUBE_CONFIG} /vagrant/
-
-echo -e "\033[1;3;34m--- K3s server installation complete on pbureeraS ---\033[0m"
+echo -e "\033[1;3;34m--- Server installation complete on pbureeraS ---\033[0m"
--- p1/scripts/setupWorker.sh
+++ p1/scripts/setupWorker.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-echo -e "\033[1;3;34m--- Server Worker script starting ---\033[0m"
+echo -e "\033[1;3;34m--- Worker script starting ---\033[0m"
 
-apk update && apk add curl && apk add net-tools
+apt-get update && apt-get install -y curl net-tools
 
-ip addr add 192.168.56.111/24 dev eth1
-ip link set eth1 up
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent --node-ip=192.168.56.111 --server=https://192.168.56.110:6443 --flannel-iface=eth1 --token-file=/vagrant/node-token" sh -
 
-echo -e "\033[1;32m--- Installing K3s ---\033[0m"
-
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent --write-kubeconfig-mode=644 --node-ip=192.168.56.111 --flannel-iface=eth1 --server=https://192.168.56.110:6443 --token-file=/vagrant/node-token" sh -
-sleep 10
-
-echo -e "\033[1;3;34m--- K3s worker installation complete on pbureeraSW ---\033[0m"
+echo -e "\033[1;3;34m--- Worker installation complete on pbureeraSW ---\033[0m"
